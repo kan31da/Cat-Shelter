50414f9f81f5dd24e85fb233a813eb861684a67d
kan31da <vladislavvladovski@gmail.com>
2026-06-25 11:28:04 +0300

    add site css

--- src/index.js
+++ src/index.js
@@ -4,6 +4,13 @@ import fs from 'fs/promises';
 
 const server = http.createServer(async (req, res) => {
 
+    if (req.url === '/content/styles/site.css') {
+        const cssFile = await fs.readFile('./src/content/styles/site.css', 'utf-8');
+        res.writeHead(200, { 'Content-Type': 'text/css' });
+        res.write(cssFile);
+        return res.end();
+    }
+
     const homePage = await fs.readFile('./src/views/home/index.html', 'utf-8');
 
     res.writeHead(200, { 'Content-Type': 'text/html' });
