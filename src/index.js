84278db174930e420274df17d75937236605a96f
kan31da <vladislavvladovski@gmail.com>
2026-06-25 11:19:32 +0300

    add home page html

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import http from 'http';
 
-const server = http.createServer((req, res) => {
-    res.write('Hello, World!');
+import fs from 'fs/promises';
+
+const server = http.createServer(async (req, res) => {
+
+    const homePage = await fs.readFile('./src/views/home/index.html', 'utf-8');
+
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(homePage);
     res.end();
 });
 
